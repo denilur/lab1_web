43debf68130aea466eed934aa14b85422272fefe
Anton Dranitsyn <a.dranitsyn@citydrive.ru>
2023-07-11 00:04:23 +0300

    requirements

--- flaskapp/net.py
+++ flaskapp/net.py
@@ -30,13 +30,16 @@ resnet = keras.applications.resnet_v2.ResNet50V2(include_top=True,
 
 
 def read_image_files(files_max_count, dir_name):
-    files = os.listdir(dir_name)
+    files = [item.name for item in os.scandir(dir_name) if item.is_file()]
     files_count = files_max_count
-    if (files_max_count > len(files)):  # определяем количество файлов не больше max
+
+    if (files_max_count > len(files)):
         files_count = len(files)
     image_box = [[]]*files_count
-    for file_i in range(files_count):  # читаем изображения в список
-        image_box[file_i] = Image.open(dir_name+'/'+files[file_i])  # / ??
+
+    for file_i in range(files_count):
+        image_box[file_i] = Image.open(dir_name+'/'+files[file_i])
+
     return files_count, image_box
 
 
